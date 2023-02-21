43d5104ed19cfef81ff9cfe47f0bbd47c234a006
Eloagbawe <eloagbawe@gmail.com>
2023-02-22 00:40:07 +0100

    update task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,14 +9,18 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initialization function"""
         super().__init__()
+        self.cache_keys = []
 
     def put(self, key, item):
         """the put method"""
         if key is not None and item is not None:
             self.cache_data[key] = item
+            self.cache_keys.append(key)
+
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first = min(self.cache_data.keys())
+                first = self.cache_keys[0]
                 del self.cache_data[first]
+                self.cache_keys.pop(0)
                 print("DISCARD: {}".format(first))
 
     def get(self, key):
