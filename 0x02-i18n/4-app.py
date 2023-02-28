76efad5acf3a4ccd2d426d032557651ecedfea23
Eloagbawe <eloagbawe@gmail.com>
2023-02-28 23:22:15 +0100

    add solution to task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -34,7 +34,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def hello_world():
     """This function executes when the 0.0.0.0:5000/ is requested"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
